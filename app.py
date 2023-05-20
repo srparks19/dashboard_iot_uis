12a773945ccfe8a6a15f17f435bf7cc40d7c082c
srparks19 <shanlonp4@gmail.com>
2023-05-20 17:58:42 -0500

    [DIOT007]: Correciones en la estructura de la pagina

--- app.py
+++ app.py
@@ -6,20 +6,21 @@ app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['iot']
 collection = db['messages']
+# Realiza la consulta a la base de datos
+pipeline = [
+    {"$group": {"_id": "$topic", "values": {"$push": {"$objectToArray": "$values"}}}}
+]
+result = list(collection.aggregate(pipeline))
+topics_values = [{topic['_id']: [value['k'] for value in topic['values'][0]]} for topic in result]
 
 @app.route('/')
 def index():
-    # Realiza la consulta a la base de datos
-    pipeline = [
-        {"$group": {"_id": "$topic", "values": {"$push": {"$objectToArray": "$values"}}}}
-    ]
-    result = list(collection.aggregate(pipeline))
-    topics_values = [{topic['_id']: [value['k'] for value in topic['values'][0]]} for topic in result]
+    
     return render_template('index.html', topics_values=topics_values)
 
 @app.route('/sensor')
 def buttons():
-    return render_template('sensor.html')
+    return render_template('sensor.html', topics_values=topics_values)
 
 if __name__ == '__main__':
     app.run(debug=True)
