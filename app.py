700c9c0c99ed4e68eb991b19070d1f6ee52bae19
srparks19 <shanlonp4@gmail.com>
2023-05-20 17:29:00 -0500

    [DIOT006]: Consulta a la base de datos

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['iot']
+collection = db['messages']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Realiza la consulta a la base de datos
+    pipeline = [
+        {"$group": {"_id": "$topic", "values": {"$push": {"$objectToArray": "$values"}}}}
+    ]
+    result = list(collection.aggregate(pipeline))
+    topics_values = [{topic['_id']: [value['k'] for value in topic['values'][0]]} for topic in result]
+    return render_template('index.html', topics_values=topics_values)
 
 @app.route('/sensor')
 def buttons():
