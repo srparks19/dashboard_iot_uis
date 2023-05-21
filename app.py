4046ee6152239fd9714b7534d842700f6710c0f0
srparks19 <shanlonp4@gmail.com>
2023-05-21 12:01:25 -0500

    [DIOT008]: implementacion back

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -15,17 +15,43 @@ topics_values = [{topic['_id']: [value['k'] for value in topic['values'][0]]} fo
 
 @app.route('/')
 def index():
-    
     return render_template('index.html', topics_values=topics_values)
 
-@app.route('/sensor')
-def sensor():
-    # Datos de ejemplo para la gráfica
+@app.route('/sensor/<estacion>/<dispositivo>')
+def sensor(estacion,dispositivo):
+
+    # Realizar la consulta a la base de datos para obtener los documentos del tema (topic) específico
+    documentos = collection.find({'topic': estacion})
+
+    # Crear una lista para almacenar los pares de timeStamp y values
+    lista_time = []
+    lista_value = []
+    lista_resultados = []
+    data_mapa = []
+    latitud = 7.131811
+    longitud = -73.120741
+
+    # Iterar sobre los documentos y extraer los pares de timeStamp y values
+    for documento in documentos:
+        timestamp = documento['timeStamp']
+        values = documento['values'][dispositivo]
+        deviceUUID = documento['deviceUUID']
+        lista_time.append(timestamp)
+        lista_value.append(values)
+        lista_resultados.append({'timeStamp': timestamp, 'values': values, 'deviceUUID':deviceUUID})
+        data_mapa.append({'lon': longitud, 'lat': latitud, 'value':values})
+        longitud+=0.000110
+        latitud+=0.000110
+    
+    maximoNivel = max(lista_value)
+
+    # Datos de para la gráfica
     data = {
-        'timeStamp': ['2023-05-01', '2023-05-02', '2023-05-03', '2023-05-04', '2023-05-05'],
-        'values': [10, 20, 15, 25, 30]
+        'timeStamp': lista_time,
+        'values': lista_value
     }
-    return render_template('sensor.html', topics_values=topics_values, data=data)
+    return render_template('sensor.html', topics_values=topics_values, data=data, estacion=estacion, 
+                           dispositivo=dispositivo, lista_resultados=lista_resultados, maximoNivel=maximoNivel, data_mapa=data_mapa)
 
 if __name__ == '__main__':
     app.run(debug=True)
